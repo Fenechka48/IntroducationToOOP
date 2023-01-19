4acd4d6d03cdc6ffd91d4ae14347e127751a3895
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-01-19 14:17:55 +0300

    distance

--- IntroducationToOOP/main.cpp
+++ IntroducationToOOP/main.cpp
@@ -8,12 +8,40 @@ class Point // Содавая структуру или класс, мы создаем новый тип данных
 	double x;
 	double y;
 public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 
+	//                          Methods:
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
 };
+//#define STRUCT_POINT
 
 void main()
 {
 	setlocale (LC_ALL, "");
+
+
+#ifdef STRUCT_POINT
 	Point A;// объявление переменной 'A' типа Point
 	        // объявление объекта 'A' структуры Point
 	        //создание экземпляра 'A' структуры Point
@@ -24,4 +52,14 @@ void main()
 
 	Point* pA = &A;
 	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+
+	Point A;
+	A.set_x (2);
+	A.set_y (3);
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
 }
