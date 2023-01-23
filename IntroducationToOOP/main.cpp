5bdf29d603043ad23fae621e511ba6ddd1468962
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-01-23 21:22:38 +0300

    constructor_check

--- IntroducationToOOP/main.cpp
+++ IntroducationToOOP/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 using std::cout;
 using std::endl;
 using std::cin;
-
+#define delimiter "\n------------------------------------------\n"
 class Point // Содавая структуру или класс, мы создаем новый тип данных
 	         // классы и структуры еще называют пользовательскими или составными типами данных
 {
@@ -51,9 +51,16 @@ public:
 	{
 		cout << "Distructor:\t" << this << endl;
 	}
-
+	//                          Operators:
+	Point operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << " CopyAssignment:\t" << this << endl;
+		return *this;
+	}
 	//                          Methods:
-	double distance(Point other) const
+	double distance(const Point& other) const
 	{
 		double x_distance = this->x - other.x;
 		double y_distance = this->y - other.y;
@@ -75,7 +82,9 @@ double distance(Point A, Point B)
 }
 //#define STRUCT_POINT
 //#define DISTANCE_CHECK
-
+//#define CONSTRACTOR_CHECK
+//# define ASSIGNMENT_CHECK_1
+# define ASSIGNMENT_CHECK_2
 void main()
 {
 	setlocale (LC_ALL, "");
@@ -93,20 +102,24 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
 #ifdef DISTANCE_CHECK
-
-
 	Point A;
 	A.set_x (2);
 	A.set_y (3);
 	Point B;
 	B.set_x(7);
 	B.set_y(8);
+	cout << delimiter << endl;
 	cout << A.get_x() << "\t" << A.get_y() << endl;
 	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние от точки B до точки A: " << B.distance(A) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между точками А и В: " << distance(A, B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между точками B и A: " << distance(B, A) << endl;
+	cout << delimiter << endl;
 #endif // DISTANCE_CHECK
+#ifdef CONSTRACTOR_CHECK
 
 	Point A;// Default constracter
 	//cout << A.get_x() << "\t" << A.get_y() << endl;
@@ -114,6 +127,38 @@ void main()
 	Point B(2, 3);
 	B.print();
 	Point C = 4; //Single-Argument constructor
+	C.print();
+	Point D = C;//copy constractor
+	D.print();
+	
+
+	Point(const Point & other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstractor: " << this << endl;
+	}
+	~Point()
+	{
+		cout << "Distructor:\t" << this << endl;
+	}
+
+#endif // CONSTRACTOR_CHECK
+#ifdef ASSIGNMENT_CHECK_1
 
 
+	//CopyAssignment
+	Point A(2, 3);
+	A.print();
+	Point B = A;     //CopyConstructor
+	B.print();
+	Point C;
+	C = B;           //Assignment
+	C.print();
+#endif // ASSIGNMENT_CHECK_1
+
+	int a, b, c;
+	a = b = c = 0;
+	Point A, B, C;
+	A = B = C = Point(2, 3);
 }
