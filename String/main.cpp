0432c74512d275a5100d673ae0a8d260c19f63fe
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-02-06 21:19:20 +0300

    new project

---
+++ String/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class String
+{
+	int size;     // размер строки
+	char* str;   // адрес строки в динамической памяти
+public:
+	int get_size() const
+	{
+		return size;
+	}
+	const char* get_str() const
+	{
+		return str;
+	}
+	//       Constructors:
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefConstractuctor:\t" << this << endl;
+	}
+	~String()
+	{
+		delete this->str;
+		cout << "Destructor:\t" << this << endl;
+	}
+	//       Methods:
+	void print() const
+		{
+			cout << "Size:\t" << size << endl;
+	        cout << "Str:\t" << str << endl;
+		}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	String str1(5);
+	str1.print();
+}
