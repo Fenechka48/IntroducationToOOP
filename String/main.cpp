7c41a50b3241e4aeb33e585905b0ec9d6119161d
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-02-14 09:38:27 +0300

    String done

--- String/main.cpp
+++ String/main.cpp
@@ -1,135 +1,194 @@
-﻿#include <iostream>
+﻿#include<iostream>
 using namespace std;
-#define delimiter "\n-----------------------------------------------\n"
-class String;
-String operator +=(const String& left, const String& right);
+using std::cin;
+using std::cout;
+using std::endl;;
 
+class String;
+String operator+(const String& left, const String& right);
 
 class String
 {
-	int size;     // размер строки
-	char* str;   // адрес строки в динамической памяти
+	int size;		//размер строки в Байтах
+	char* str;		//адрес строки в динамической памяти
 public:
-	int get_size() const
+	int get_size()const
 	{
 		return size;
 	}
-	const char* get_str() const
+	const char* get_str()const
 	{
 		return str;
 	}
-	char* get_str() 
+	char* get_str()
 	{
 		return str;
 	}
-	//       Constructors:
-	
+
+	//				Constructors:
 	explicit String(int size = 80)
 	{
 		this->size = size;
 		this->str = new char[size] {};
-		cout << "DefConstractuctor:\t" << this << endl;
+		cout << "DefConstructor:\t" << this << endl;
 	}
-	String (const char* str)
+	String(const char* str)
 	{
-		this->size = strlen(str)+1;        //функция возвращает размер в символах, но в классе хранится размер в байтах и терминирующего символа
+		this->size = strlen(str) + 1;	//функция strlen() возвращает размер строки в символах,
+										//но в классе хранится размер строки в Байтах, т.е., с учетом терминирующего нуля.
 		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)
-			this->str[i] = str[i];
-		cout << "1ArgConstractuctor:\t" << this << endl;
+		for (int i = 0; i < size; i++)this->str[i] = str[i];
+		cout << "1ArgConstructor:" << this << endl;
 	}
-	
 	String(const String& other)
 	{
+		//Deep copy - побитовое копирование
 		this->size = other.size;
-		this->str = new char [size] {};
+		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)
 			this->str[i] = other.str[i];
-		cout << "Copy_constractor:\t " << this << endl;
+		cout << "CopyConstructor:\t" << endl;
+	}
+	String(String&& other)noexcept
+	{
+		//Shallow copy - поверхностное копирование
+		this->size = other.size;
+		this->str = other.str;		//Shallow copy
+		other.size = 0;
+		other.str = nullptr;		//nullptr - это указатель на ноль.
+		cout << "MoveConstructor:" << this << endl;
 	}
 	~String()
 	{
 		delete this->str;
 		cout << "Destructor:\t" << this << endl;
 	}
-	//    Operators
+
+	//				Operators:
 	String& operator=(const String& other)
 	{
-		if (this == &other) return *this;
+		//			l-value = r-value;
+		if (this == &other)return *this;
 		delete[] this->str;
 		this->size = other.size;
-		this->str = new char [size] {};
+		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)
 			this->str[i] = other.str[i];
 		cout << "CopyAssignment:\t" << this << endl;
 		return *this;
 	}
-	String& operator +=(const String& other)
+	String& operator=(String&& other)
 	{
-		return *this = *this +other;
+		this->size = other.size;
+		this->str = other.str;
+		other.size = 0;
+		other.str = nullptr;
+		cout << "MoveAssignment:\t" << this << endl;
+		return *this;
+	}
+
+	String& operator+=(const String& other)
+	{
+		return *this = *this + other;
+	}
+
+	const char& operator[](int i)const
+	{
+		return str[i];
 	}
 	char& operator[](int i)
 	{
 		return str[i];
 	}
-	char& operator[](int i) const
-	{
-		return str[i];
-	}
-	//                TYPE-cast operators
-	explicit operator char*()const
-	{
-		return String(*this).str;
-	}
-	
 
-	//       Methods:
-	void print() const
-		{
-			cout << "Size:\t" << size << endl;
-	        cout << "Str:\t" << str << endl;
-		}
+
+
+	//				Methods:
+	void print()const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+	}
 };
 
-std::ostream& operator << (std::ostream& os, const String& obj)
+String operator+(const String& left, const String& right)
+{
+	String cat(left.get_size() + right.get_size() - 1);
+	for (int i = 0; i < left.get_size(); i++)
+		cat[i] = left[i];
+	//cat.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		cat[i + left.get_size() - 1] = right[i];
+	//cat.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	return cat;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
-String operator+ (const String& left, const String& right)
-{
-	String cat (left.get_size() + right.get_size() - 1);
-	for (int i = 0; i < left.get_size(); i++)
-		cat[i] = left[i];
-	for (int i = 0; i < right.get_size(); i++)
-		cat[i + left.get_size() - 1] = right.get_str()[i];
-	return cat;
 
-}
+//#define BASE_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	/*String str1(5);
-	str1.print();*/
+#ifdef BASE_CHECK
+	//String str1(5);
+//str1.print();
 
-	String str=("Hello");
-	str = str;
-	cout << str << endl;
+	String str1 = "Hello";	//"Hello" - это строковая константа
+	str1 = str1;
+	cout << str1 << endl;
 
-	str.print();
-	cout << delimiter << endl;
+	String str2 = "World";
+	cout << str2 << endl;
 
-	String str1 = ("World");
-	str1.print();
-	cout << delimiter << endl;
+	//cout << "\n-----------------------------------------\n";
+	//str1 += str2;
+	//cout << str1 << endl;
+	//cout << "\n-----------------------------------------\n";
 
-	str += str1;
-	cout << str << endl;
-	/*String str3;
-	str3 = str + str1;
+	String str3;
+	str3 = str1 + str2;	//Move constructor
 	cout << str3 << endl;
-	cout << delimiter << endl;
 
-	str1 = str3;
-	cout << str1 << endl;*/
+	String str4 = str3;	//Copy constructor
+
+	//str1 = str3;
+	//cout << str1 << endl;
+
+	/*
+	Deep copy:
+	CopyConstructor;
+	CopyAssignment
+	------------------------------
+	Shallow copy:
+	MoveConstructor
+	MoveAssignment
+	other
+	Move semantic C++11, C++03
+	R-value reference
+	------------------------------
+	*/
+#endif // BASE_CHECK
+
+	String str1;	//Default constructor
+	str1.print();
+	String str2 = "Hello";	//Single argument constructor
+	str2.print();
+	String str3 = str2;	//Copy constructor
+	str3.print();
+	String str4();	//Здесь не вызывается никакой конструктор, потому что НЕ создается объект,
+					//Здесь объявлется функция str4(), которая не принимает никаких параметров,
+					//и возвращает значение типа String.
+	//str4.print();	//str4 - это НЕ объект, это функция, а для функции нельзя вызвать метод.
+	//Если мы хотим явно вызвать конструктор по умолчанию, то это можно сделать так:
+	String str5{};	//Явный вызов конструктора по умолчанию.
+	str5.print();
+	String str6{ 22 };
+	str6.print();
+	String str7{ "World" };
+	str7.print();
+	String str8{ str7 };	//Copy constructor
 }
